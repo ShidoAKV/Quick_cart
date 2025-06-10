fdca0070fd80ad944260a5ffde7d030d73ae662a
abhishek <abhi3vish@gmail.com>
2025-06-10 12:04:03 +0530

    modified the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
     }`;
 
   return (
-    <nav className="flex items-center shadow-md shadow-gray-600 lg:shadow-none justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700 sticky top-0 z-50 lg:static lg:max-h-screen lg:overflow-y-auto bg-white">
+    <nav className="flex items-center  justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700 sticky top-0 z-50 lg:static lg:max-h-screen lg:overflow-y-auto bg-white">
       <Image
         className="cursor-pointer rounded-sm w-10 md:w-10 bg-gradient-to-r from-black to-green-800 p-0.5"
         onClick={() => router.push("/")}
